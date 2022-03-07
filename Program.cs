0eb0ca9d50397eaffdd6b74134ff7b9c4cd4b25d
JC Bridges <44345503+jennciccoli@users.noreply.github.com>
2022-03-06 16:55:25 -0800

    Create Program.cs
    Created the program for "Console Creatures"

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /* Prints ._. to the console */
+      Console.WriteLine(" .-.");    
+      Console.WriteLine(" (o o)");
+      Console.WriteLine(" | 0 |");
+     }
+  }
+}
